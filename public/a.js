033ad3f88ab5031f3a0c27a07074e7866788ddd6
myself151 <qka@ymail.ne.jp>
2025-09-19 17:08:56 +0900

    Update a.js

--- public/a.js
+++ public/a.js
@@ -1,5 +1,1 @@
-const socket = io();
-const numEl = document.getElementById("number");
-document.getElementById("next").onclick = ()=>socket.emit("next");
-document.getElementById("prev").onclick = ()=>socket.emit("prev");
-socket.on("updateNumber", n=>numEl.textContent=n);
+
